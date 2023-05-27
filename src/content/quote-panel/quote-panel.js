204334e9bb5e8c5d5896dfa0865c78f69578d2d2
njcushing <129661299+njcushing@users.noreply.github.com>
2023-05-27 11:17:19 +0100

    Create div inside quotePanel for quote text specifically

--- src/content/quote-panel/quote-panel.js
+++ src/content/quote-panel/quote-panel.js
@@ -1,10 +1,15 @@
 const quotePanel = () => {
     const panel = document.createElement("div");
     panel.classList.add("quote-panel");
+
+    const quote = document.createElement("div");
+    quote.classList.add("quote-panel-quote");
+    panel.appendChild(quote);
+
     return panel;
 };
 const quotePanelUpdateQuoteText = (e, text) => {
-    e.textContent = text;
+    e.childNodes[0].textContent = text;
 };
 export default quotePanel;
 export { quotePanelUpdateQuoteText };
