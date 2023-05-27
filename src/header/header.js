a511478f21ae19934677337a02292c9a928e89b4
njcushing <129661299+njcushing@users.noreply.github.com>
2023-05-27 10:10:28 +0100

    Move style rules to styles.css

--- src/header/header.js
+++ src/header/header.js
@@ -2,17 +2,15 @@ import headerNavButton from "./header-nav-button.js";
 
 const pageHeader = () => {
     const header = document.querySelector(".header");
-    header.style.display = "grid";
-    header.style.gridTemplateAreas = "repeat(2, auto) / 1fr";
-    header.style.backgroundColor = "rgb(230, 6, 6)";
-    header.style.boxShadow = "6px 6px 5px rgba(60, 60, 60, 0.3)";
 
     const companyName = document.createElement("h1");
-    companyName.classList.add("test");
+    companyName.classList.add("company-name");
     companyName.textContent = "Baker's Dozen";
-    companyName.style.textAlign = "center";
-    companyName.style.fontSize = "1.8rem";
-    companyName.style.color = "white";
     header.appendChild(companyName);
+
+    const buttonHome = headerNavButton();
+    companyName.textContent = "Home";
+
+    header.appendChild(buttonHome);
 };
 export default pageHeader;
