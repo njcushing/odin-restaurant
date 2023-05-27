04d0ccb2ffd41236518b9636ad386dabb7ca0454
njcushing <129661299+njcushing@users.noreply.github.com>
2023-05-27 10:52:36 +0100

    Remove sidebar from page layout; don't need it

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -7,10 +7,6 @@ const pageLayout = () => {
     header.classList.add("header");
     content.appendChild(header);
 
-    const sidebar = document.createElement("div");
-    sidebar.classList.add("sidebar");
-    content.appendChild(sidebar);
-
     const main = document.createElement("div");
     main.classList.add("main");
     content.appendChild(main);
