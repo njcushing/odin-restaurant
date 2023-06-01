cee062888fa47e15666bf9cf770074c5bd3e5989
njcushing <129661299+njcushing@users.noreply.github.com>
2023-06-01 12:00:57 +0100

    Create opening times panel for contact tab

--- src/content/content-contact.js
+++ src/content/content-contact.js
@@ -1,4 +1,64 @@
+import openingTimesPanel from "./opening-times-panel.js";
 import styles from "./content-contact-styles.lazy.css";
 
-const contentContact = () => {};
+let drawnState = false;
+
+const contentContact = () => {
+    let container;
+
+    const draw = () => {
+        if (drawnState) return;
+        drawnState = !drawnState;
+
+        const content = document.querySelector(".content");
+        if (!content) return;
+
+        container = document.createElement("div");
+        container.classList.add("contact-content-container");
+        content.appendChild(container);
+
+        const openingTimesContainer = document.createElement("div");
+        openingTimesContainer.classList.add("opening-times-container");
+        container.appendChild(openingTimesContainer);
+
+        const openingTimesTitle = document.createElement("h2");
+        openingTimesTitle.classList.add("opening-times-title");
+        openingTimesTitle.textContent = "Opening Times";
+        openingTimesContainer.appendChild(openingTimesTitle);
+
+        const openingTimes = openingTimesPanel();
+        openingTimes.setDayTimes(0, "07:30am", "21:00pm");
+        openingTimes.setDayTimes(1, "07:30am", "21:00pm");
+        openingTimes.setDayTimes(2, "08:30am", "20:00pm");
+        openingTimes.setDayTimes(3, "07:30am", "21:00pm");
+        openingTimes.setDayTimes(4, "07:30am", "21:00pm");
+        openingTimes.setDayTimes(5, "06:00am", "22:30pm");
+        openingTimes.setDayTimes(6, "09:00am", "17:00pm");
+        openingTimesContainer.appendChild(openingTimes.e);
+
+        const addressContainer = document.createElement("div");
+        addressContainer.classList.add("address-container");
+        container.appendChild(addressContainer);
+
+        const contactInfoContainer = document.createElement("div");
+        contactInfoContainer.classList.add("contact-information-container");
+        container.appendChild(contactInfoContainer);
+
+        styles.use();
+    };
+
+    const clear = () => {
+        if (!drawnState) return;
+        drawnState = !drawnState;
+
+        container.remove();
+
+        styles.unuse();
+    };
+
+    return {
+        draw,
+        clear,
+    };
+};
 export default contentContact;
---
+++ src/content/opening-times-panel.js
@@ -0,0 +1,76 @@
+const openingTimesPanel = (classes = []) => {
+    const e = document.createElement("div");
+    e.classList.add("opening-times-panel");
+    classes.forEach((cls) => {
+        e.classList.add(cls);
+    });
+
+    const setDayTimes = (day, openingTime, closingTime) => {
+        if (day < 0 || day > 6) return;
+        days[day].childNodes[1].textContent = openingTime;
+        days[day].childNodes[3].textContent = closingTime;
+    };
+
+    const setDayClosed = (day) => {};
+
+    let days = [];
+    for (let i = 0; i < 7; i++) {
+        const day = document.createElement("div");
+        day.classList.add("opening-times-panel-day");
+        e.appendChild(day);
+
+        const dayName = document.createElement("div");
+        dayName.classList.add("opening-times-panel-day-name");
+        day.appendChild(dayName);
+
+        const dayOpeningTime = document.createElement("div");
+        dayOpeningTime.classList.add("opening-times-panel-day-opening-time");
+        day.appendChild(dayOpeningTime);
+
+        const dayTimeSeparator = document.createElement("div");
+        dayTimeSeparator.classList.add("opening-times-panel-day-time-sep");
+        dayTimeSeparator.textContent = "-";
+        day.appendChild(dayTimeSeparator);
+
+        const dayClosingTime = document.createElement("div");
+        dayClosingTime.classList.add("opening-times-panel-day-closing-time");
+        day.appendChild(dayClosingTime);
+
+        switch (i) {
+            case 0:
+                dayName.textContent = "Monday: ";
+                break;
+            case 1:
+                dayName.textContent = "Tuesday: ";
+                break;
+            case 2:
+                dayName.textContent = "Wednesday: ";
+                break;
+            case 3:
+                dayName.textContent = "Thursday: ";
+                break;
+            case 4:
+                dayName.textContent = "Friday: ";
+                break;
+            case 5:
+                dayName.textContent = "Saturday: ";
+                break;
+            case 6:
+                dayName.textContent = "Sunday: ";
+                break;
+            default:
+                dayName.textContent = "Error!";
+        }
+
+        days.push(day);
+
+        setDayTimes(i, "00:00am", "00:00am");
+    }
+
+    return {
+        e,
+        setDayTimes,
+        setDayClosed,
+    };
+};
+export default openingTimesPanel;
